e4ef5a279ec19c5668e110473edf50cfb22a33a9
Will Pinkston <william@pinkston.com>
2016-10-04 02:26:41 -0600

    1.2 first write-up of postfixEval func

--- Student_Code/Exp_Manager.cpp
+++ Student_Code/Exp_Manager.cpp
@@ -2,6 +2,7 @@
 #include "Exp_Manager.h"
 #include <iostream>
 //------------------------------------------------------------------------
+// utility functions
 void printStack (stack<char> writeIt)
 {
   if (writeIt.size() == 0)
@@ -39,7 +40,6 @@ bool Exp_Manager::isBalanced(string expression)
       {
           checker.push(c);
           std::cout << "pushed" << c << endl;
-          printStack(checker);
       }
       else if (c == '}')
       {
@@ -47,7 +47,6 @@ bool Exp_Manager::isBalanced(string expression)
           {
               checker.pop();
               std::cout << "popped" << c << endl;
-              printStack(checker);
           }
           else
           {
@@ -60,7 +59,6 @@ bool Exp_Manager::isBalanced(string expression)
           {
               checker.pop();
               std::cout << "popped" << c << endl;
-              printStack(checker);
           }
           else
           {
@@ -73,7 +71,6 @@ bool Exp_Manager::isBalanced(string expression)
           {
               checker.pop();
               std::cout << "popped" << c << endl;
-              printStack(checker);
           }
           else
           {
@@ -110,6 +107,63 @@ string Exp_Manager::infixToPostfix(string infixExpression)
 //------------------------------------------------------------------------
 string Exp_Manager::postfixEvaluate(string postfixExpression)
 {
-  return "";
+    /*
+     * Evaluates a postfix expression returns the result as a string
+     *
+     * - The given postfix expression will have a space between every number or operator.
+     * - Check lab requirements for what will be considered invalid.
+     *
+     * return the string "invalid" if postfixExpression is not a valid postfix Expression
+     * otherwise, return the correct evaluation as a string
+     */
+    
+    stringstream ss;
+    ss << postfixExpression;
+    char c;
+    stack<char> calc;
+    while (ss >> c)
+    {
+        if (c == '+' || c == '-' || c == '*' || c == '/')
+        {
+            //should these be ints or chars?
+            int second = calc.top();
+            calc.pop();
+            int first = calc.top();
+            calc.pop();
+            char result;
+            if (c == '+')
+            {
+                calc.push(result = first + second);
+            }
+            else if (c == '-')
+            {
+                calc.push(result = first - second);
+            }
+            else if (c == '*')
+            {
+                calc.push(result = first * second);
+            }
+            else if (c == '/')
+            {
+                calc.push(result = first / second);
+            }
+        }
+        else
+        {
+            if (isdigit(c))
+            {
+                calc.push(c);
+            }
+        }
+    }
+    stringstream ss2;
+    ss2 << calc.top();
+    return ss2.str();
 }
 //------------------------------------------------------------------------
+
+
+
+
+
+
